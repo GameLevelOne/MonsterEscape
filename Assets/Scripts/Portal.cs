79d12356b3e41ac0b518eb49625a2acd5e9b8d6b
Helga <helga@gamelevelone.com>
2017-05-02 17:01:00 +0700

    create newRoom

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour {
+	public RoomLoadController roomLoadController;
+	public string roomName;
+
+	void OnTrigger2DEnter (Collider2D col)
+	{
+		if (col.tag == "Player") {
+			if (roomName == "roomLeft") {
+				Debug.Log ("roomLeft");
+			} else if(roomName == "roomRight"){
+				Debug.Log("roomRight");
+			}
+		}
+	}
+}
