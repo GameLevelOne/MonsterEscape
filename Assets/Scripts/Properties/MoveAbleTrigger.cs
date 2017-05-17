025d78b239f51c55198378a06fb9a640c4185a50
Dennis <dennis@gamelevelone.com>
2017-05-17 16:04:42 +0700

    changed npc sprite, modified some rooms

--- Assets/Scripts/Properties/MoveAbleTrigger.cs
+++ Assets/Scripts/Properties/MoveAbleTrigger.cs
@@ -3,28 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveAbleTrigger : MonoBehaviour {	
-	public PlayerMoveSide pos;
-	MoveAble parent;
-	void Start(){
-		parent = transform.parent.GetComponent<MoveAble>();
-	}
-
-	void OnTriggerEnter2D(Collider2D p){
-		if(p.tag == "Player"){
-			//register OnPlayerAction
-			parent.playerPullPos = pos;
-			parent.player = p.transform;
-		}
-	}
-
-	void OnTriggerExit2D(Collider2D p){
-		if(p.tag == "Player"){
-			//unregister OnPlayerAction
-			parent.player = null;
-		}
-	}
-
-	void OnPlayerAction(){
-		parent.OnPlayerAction();
-	}
+	
 }
