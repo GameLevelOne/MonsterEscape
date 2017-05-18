a1025393d2402a202999ba158f634cce5e855e10
Helga <helga@gamelevelone.com>
2017-05-18 17:51:07 +0700

    add title screen

---
+++ Assets/Scripts/Room/SceneTitleManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTitleManager : MonoBehaviour {
+	public Fader fader;
+
+	// Use this for initialization
+	void Start () {
+		fader.FadeIn();
+	}
+	
+	void OnEnable(){
+		Fader.OnFadeOutFinished += ReadyToChangeScene;
+	}
+
+	void OnDisable(){
+		Fader.OnFadeOutFinished -= ReadyToChangeScene;
+	}
+
+	void ReadyToChangeScene(){
+		SceneManager.LoadScene("RoomRoot");
+	}
+
+	public void OnPanelClick (){
+		fader.FadeOut();
+	}
+}
